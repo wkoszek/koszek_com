00305b3b147640385e2dd6e3941ccf73905640a1
Wojciech Adam Koszek <wojciech@koszek.com>
2017-02-14 00:12:32 -0800

    Put a simple unit testing in place.

--- test.rb
+++ test.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+# Copyright (c) 2017 Wojciech Adam Koszek <wojciech@koszek.com>
+
+# This file is a unit-test for the website. I use it to make sure my
+# infrastructure configuration has been deployed correctly. Basically
+# I try to make sure that domains which are under my control are properly
+# showing my main website.
 
 require 'minitest/autorun'
 require 'net/http'
@@ -8,15 +14,36 @@ require 'json'
 class TestMeme < Minitest::Test
   def setup
     @redir_resp_exp = [301, "https://www.koszek.us/"]
+    @debug = 0
+  end
+
+  def http_response_debug(response)
+    if @debug < 1 then
+      return
+    end
+    case response
+    when Net::HTTPSuccess then
+      print "# success"
+      response
+    when Net::HTTPRedirection then
+      location = response['location']
+      warn "# redirected to #{location}"
+    else
+      response.value
+    end
   end
 
   def http_code_for_url(url_str)
-    url = URI.parse(url_str)
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    return [ res.code.to_i, res['location']]
+    uri = URI(url_str)
+    use_ssl = (url_str =~ /^https/)
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => use_ssl) do |http|
+      req = Net::HTTP::Get.new uri
+      http.request(req) # Net::HTTPResponse object
+    end
+
+    http_response_debug(response)
+
+    return [ response.code.to_i, response['Location']]
   end
 
   def test_koszek_redirect_http
@@ -27,9 +54,9 @@ class TestMeme < Minitest::Test
   end
 
   def test_koszek_redirect_https
-    #assert_equal @redir_resp_exp, http_code_for_url("https://koszek.co")
+    assert_equal @redir_resp_exp, http_code_for_url("https://koszek.co")
     assert_equal @redir_resp_exp, http_code_for_url("https://koszek.tv")
-    #assert_equal @redir_resp_exp, http_code_for_url("https://koszek.org")
-    #assert_equal @redir_resp_exp, http_code_for_url("https://koszek.net")
+    assert_equal @redir_resp_exp, http_code_for_url("https://koszek.org")
+    assert_equal @redir_resp_exp, http_code_for_url("https://koszek.net")
   end
 end
