888b3ece335fb78ec2dcc1d5ad0525a3800a3df8
Wojciech Adam Koszek <wojciech@koszek.com>
2017-02-22 00:22:09 -0800

    Test SSL certificate settings.

--- test.rb
+++ test.rb
@@ -9,9 +9,15 @@
 require 'minitest/autorun'
 require 'net/http'
 
+require 'pp'
+require 'json'
+require 'socket'
+require 'openssl'
+
 class TestMeme < Minitest::Test
   def setup
-    @redir_resp_exp = [301, "https://www.koszek.com/"]
+    @domain_main = "koszek.com";
+    @redir_resp_exp = [301, "https://www.#{@domain_main}/"]
     @supported_domains = [ "koszek.co", "koszek.tv", "koszek.us", "koszek.org", "koszek.net"]
     @debug = 0
   end
@@ -56,6 +62,42 @@ class TestMeme < Minitest::Test
     @supported_domains.each do |domain|
       assert_equal @redir_resp_exp, http_code_for_url("https://#{domain}")
       assert_equal @redir_resp_exp, http_code_for_url("http://www.#{domain}")
+    end
+  end
+
+  def ssl_get_info(domain)
+    tcp_client = TCPSocket.new(domain, 443)
+    ssl_client = OpenSSL::SSL::SSLSocket.new(tcp_client)
+    ssl_client.connect
+    cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+    ssl_client.sysclose
+    tcp_client.close
+    issuer = OpenSSL::X509::Name.new(cert.subject).to_a[0]
+    cert_domain = issuer[1]
+    certprops = OpenSSL::X509::Name.new(cert.issuer).to_a
+    issuer = certprops.select { |name, data, type| name == "O" }.first[1]
+
+    return {
+      :domain => cert_domain,
+      :not_before => cert.not_before,
+      :not_after => cert.not_after,
+      :issuer => issuer
+    }
+  end
+
+  def test_ssl_domain
+    @supported_domains.each do |supp_domain|
+      info = ssl_get_info(supp_domain)
+      assert_equal info[:domain], @domain_main
+    end
+  end
+
+  def test_ssl_cert_date
+    @supported_domains.each do |supp_domain|
+      info = ssl_get_info(supp_domain)
+      time_cur = Time.new()
+      assert_equal time_cur > info[:not_before], true
+      assert_equal time_cur < info[:not_after], true
     end
   end
 
